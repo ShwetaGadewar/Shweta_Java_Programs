7d0d724f475220ff1a2f7bfdb538b23e467d98d9
Shweta Gadewar <shwetasgadewar@gmail.com>
2024-05-05 16:00:17 +0530

    2

---
+++ AccessSpecifireEx2.java
@@ -0,0 +1,15 @@
+package BasicConcept;
+
+public class AccessSpecifireEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AccessSeEx D1=new AccessSeEx();
+		D1.add();
+		D1.div();
+		D1.mul();
+		
+		
+	}
+
+}
